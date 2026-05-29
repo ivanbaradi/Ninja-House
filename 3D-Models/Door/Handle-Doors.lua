3e2bcec3ba008500f3463c06851d8a9c58716a8c
ivanbaradi <ivanbaradi@gmail.com>
2026-05-29 00:56:02 -0700

    verified players properly

--- 3D-Models/Door/Handle-Doors.lua
+++ 3D-Models/Door/Handle-Doors.lua
@@ -1,28 +1,28 @@
---Roblox Services
+-- Roblox Services
 ReplicatedStorage = game.ReplicatedStorage
 ServerScriptService = game.ServerScriptService
 Players = game:GetService('Players')
 
---Door Handler Modules
+-- Door Handler Modules
 DoorHandler = ServerScriptService['Door Handler']
 AnimateDoor = DoorHandler['Animate Door']
 RunDoor = DoorHandler['Run Door']
 
---Game Owner's Communications
+-- Game Owner's Communications
 GameOwnerCommunications = ReplicatedStorage['Game Owner Settings'].Communications
 
---Multi-Door Model
+-- Animating Door Model
 DoorOpener = script.Parent -- part for players to touch for opening the door
 Doors = DoorOpener.Parent
 
---Prevents unneccessary functional executions
+-- Prevents unneccessary functional executions
 debounce = false
 
---Configuration
+-- Configuration
 Configuration = Doors.Configuration
 InsideDoorOpener = Configuration['Is Inside Door Opener']
 
---Inserts all door models into array to set all their animations
+-- Inserts all door models into array to set all their animations
 DoorDictionaries = {}
 for _, Door in pairs(Doors:GetChildren()) do
 	if not Door:IsA('Model') then continue end
@@ -43,13 +43,13 @@ DoorOpener.Touched:Connect(function(part: BasePart)
 	local Character = part:FindFirstAncestorOfClass('Model')
 	local Player = Players:GetPlayerFromCharacter(Character)
 	
-	--Must check if the model is a character
-	if not Character:FindFirstChild('Humanoid') then
+	-- Must check if this character is owned by an actual player
+	if not Player then
 		debounce = false
 		return 
 	end
 	
-	--Must check if other players have permission to use doors
+	-- Must check if other players have permission to use doors
 	if not GameOwnerCommunications['Players Can Use Doors']:Invoke(Player) then 
 		debounce = false
 		return 
