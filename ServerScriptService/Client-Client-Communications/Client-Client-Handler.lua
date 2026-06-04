d07ee5be169164d80b5c8aea68bcf7f7e124bfc7
ivanbaradi <ivanbaradi@gmail.com>
2026-06-04 14:14:30 -0700

    refactored code

--- ServerScriptService/Client-Client-Communications/Client-Client-Handler.lua
+++ ServerScriptService/Client-Client-Communications/Client-Client-Handler.lua
@@ -1,56 +1,36 @@
 --[[Client-to-Client Communications enables the interactions between 
-	the client, server, and another client 
-]]
+	the client, server, and another client ]]
 
---ReplicatedStorage
-local ReplicatedStorage = game.ReplicatedStorage
---Client to Client Communications (Remote Event)
-local Client_Client_EVENT = ReplicatedStorage:FindFirstChild('Client-Client Communications')
---Client to Client Communications (Remote Function)
-local Client_Client_FUNC = ReplicatedStorage:FindFirstChild('Client-Client Communications 2')
+-- Roblox Services
+ReplicatedStorage = game.ReplicatedStorage
 
+-- Client to Client Communications Systems
+ClientClientEvent = ReplicatedStorage['Client-Client Communications']
+ClientClientFunction = ReplicatedStorage['Client-Client Communications 2']
 
 
 --[[Handles a one-way passage between the client, server, and another client.
-	
-	If object is nil, then this server event will only pass the Player
-	object as the parameter. In other word, no object is passed.
 
 	Parameter(s):
-	player => player who triggered this event (Player)
-	remoteEventName => name of the remote event to trigger (string)
-	object => any data 
+		player => player who triggered this event 
+		remoteEventName => name of the remote event to trigger
+		object => any data 
 ]]
-Client_Client_EVENT.OnServerEvent:Connect(function(player: Player, remoteEventName: string, object: any)
-	
-	local TargetRemoteEvent = ReplicatedStorage:FindFirstChild(remoteEventName)
-	
-	if object ~= nil then
-		TargetRemoteEvent:FireClient(player, object)
-	else
-		TargetRemoteEvent:FireClient(player)
-	end
+ClientClientEvent.OnServerEvent:Connect(function(player: Player, remoteEventName: string, object: any)	
+	ReplicatedStorage[remoteEventName]:FireClient(player, object)
 end)
 
 
 
 --[[Handles a two-way passage between the client, server, and another client.
 	In most cases, the client that invokes the server expects a return of some
-	data from another client invoked by the server.
-	
-	If object is nil, then this server invoke will only pass the Player
-	object as the parameter. In other word, no object is passed.
+	data from another client invoked by the server unless it's a void function.
 
 	Parameter(s):
-	player => player who invoked this function (Player)
-	remoteFuncName => name of the remote function to invoke (string)
-	object => any data
+		player => player who invoked this function 
+		remoteFuncName => name of the remote function to invoke
+		object => any data
 ]]
-Client_Client_FUNC.OnServerInvoke = function(player: Player, remoteFuncName: string, object: any)
-	
-	local TargetRemoteFunction = ReplicatedStorage:FindFirstChild(remoteFuncName)
-	
-	if object ~= nil then return TargetRemoteFunction:InvokeClient(player, object) end
-	
-	return TargetRemoteFunction:InvokeClient(player)
+ClientClientFunction.OnServerInvoke = function(player: Player, remoteFuncName: string, object: any)
+	return ReplicatedStorage[remoteFuncName]:InvokeClient(player, object)
 end
